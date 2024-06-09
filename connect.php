dbe0205b193198200510280eefaf765c6af97043
onemejom <166485991+onemejom@users.noreply.github.com>
2024-06-09 20:33:09 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "slezakdb";
-$password = "Databaza1";
-$dbname = "northwind24"; 
+$username = "userdb";
+$password = "databaza";
+$dbname = "northwindmysql"; 
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
