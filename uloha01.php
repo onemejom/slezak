a2d31d80b46ce1fb9321cf56c3ca570dec694266
onemejom <166485991+onemejom@users.noreply.github.com>
2024-06-17 11:39:24 +0200

    Update uloha01.php

--- uloha01.php
+++ uloha01.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Úloha 01</title>
-    <link rel="stylesheet" href="uloha01.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
